ce95acb66075fcb84bd57715d00b6acbab656416
NguyenThiThanhHuyenn <thithanhhuyennguyen002@gmail.com>
2022-10-16 14:43:18 +0700

    Them button

--- HY_1/HY_1/Form1.Designer.cs
+++ HY_1/HY_1/Form1.Designer.cs
@@ -29,23 +29,34 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label1.Location = new System.Drawing.Point(200, 84);
+            this.label1.Location = new System.Drawing.Point(187, 94);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(280, 84);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nguyen Thi Thanh Huyen";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(267, 293);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "LAUGH";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(685, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
